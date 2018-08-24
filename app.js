d45803e6830eb8704f56b003a0884f0790b2f55c
ziw1 <111111@qq.com>
2018-08-24 14:12:07 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,65 @@
+var url_base = 'http://localhost:8081/RichMan/';
+//var url_base = 'https://ziweb.top/RichMan/';
+App({
+  onLaunch: function (options) {
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        if (res.code) {
+          //发起网络请求
+          wx.request({
+            url: url_base + 'WXManage/wxCheckLogin',    
+            data: {
+              code: res.code
+            },
+            success: function (res) {
+              wx.setStorageSync('JSESSIONID', res.data)
+            }
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
+    setInterval(this.refresh, 20 * 60 * 1000) //每20分种向后台请求，以保证session不过期
+    // 获取用户信息
+    // wx.getSetting({
+    //   success: res => {
+    //     if (res.authSetting['scope.userInfo']) {
+    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+    //       wx.getUserInfo({
+    //         success: res => {
+    //           // 可以将 res 发送给后台解码出 unionId
+    //           this.globalData.userInfo = res.userInfo
+
+    //           // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //           // 所以此处加入 callback 以防止这种情况
+    //           if (this.userInfoReadyCallback) {
+    //             this.userInfoReadyCallback(res)
+    //           }
+    //         }
+    //       })
+    //     }
+    //   }
+    // })
+  },
+  // globalData: {
+  //   userInfo: null
+  // }
+  onShow: function (options) {
+    console.log(options)
+    //将分享票据存入本地（如果不是群，则没有这玩意）
+    wx.setStorageSync('shareTicket', options.shareTicket)
+  },
+  refresh:function(){ 
+    wx.request({
+      url: url_base + 'test/connect',
+      data: {
+      },
+      header: { "Cookie": "JSESSIONID=" + wx.getStorageSync('JSESSIONID') },
+      success: function (res) {
+      }
+    })
+  }
+})
---
+++ pages/player/player.js
@@ -0,0 +1,165 @@
+var url_base = 'http://localhost:8081/RichMan/';
+//var url_base = 'https://ziweb.top/RichMan/';
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    msg : '111',
+    players : [
+      { name: 'ziw', money: 1256, openid: 'sioevmwow23fgsw' },
+      { name: 'niao', money: 6986, openid: 'ivmeo934j835i' }
+      ]
+  },
+  getMsg: function () {
+    //var seid = wx.getStorageSync('JSESSIONID');
+    var _this = this;
+    wx.request({
+      url: url_base + 'test/testSession',
+      data: {
+
+      },
+      header: { "Cookie": "JSESSIONID=" + wx.getStorageSync('JSESSIONID')},
+      success: function (res) {
+        
+        _this.setData({
+          msg: res.data
+        })
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+    wx.showShareMenu({  //开启分享按钮
+      withShareTicket: true
+
+    })
+    this.getShareInfo();
+
+    
+  },
+  getShareInfo: function () {
+    var appShareTicket = wx.getStorageSync('shareTicket');
+    if (appShareTicket != ''){
+      var _this = this;
+      wx.getShareInfo({
+        shareTicket: appShareTicket,
+        success: function (res) {
+          console.log("res====" + JSON.stringify(res))
+          //去后台解密
+          wx.request({
+            url: url_base + 'WXManage/wxDecryption',
+            data: {
+              encryptedData: res.encryptedData,
+              iv: res.iv
+            },
+            header: {
+              'content-type': 'application/json', // 默认值
+              "Cookie": "JSESSIONID=" + wx.getStorageSync('JSESSIONID')
+            },
+            success: function (res) {
+              _this.setData({
+                msg: JSON.stringify(res.data.openGId) + '123'
+                //msg: '123123'
+              })
+            }
+          })
+        },
+        fail: function (res) {
+          _this.setData({
+            text: JSON.stringify(res)
+          })
+        }
+      })
+    }
+  },
+  bindWebSocket :function(){
+    wx.connectSocket({
+      url: 'ws://127.0.0.1:8081/RichMan/websocket'
+      // url: 'wss://ziweb.top/RichMan/websocket'
+    })
+
+    wx.onSocketOpen(function (res) {
+      console.log('WebSocket连接已打开！')
+    })
+    wx.onSocketError(function (res) {
+      console.log('WebSocket连接打开失败，请检查！')
+    })
+    wx.onSocketMessage(function (res) {
+      console.log('收到服务器内容：' + res.data)
+    })
+  },
+  WXSendSocketMsg: function () {
+    console.log("WXSendSocketMsg");
+    wx.sendSocketMessage({
+      data: "balalax"
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function (res) {
+    
+    // this.onShowMsg = JSON.stringify(res);
+
+    // if (res.from === 'button') {
+    //   // 来自页面内转发按钮
+    //   console.log(res.target)
+    // }
+    return {
+      title: '自定义发标题',
+      path: '/pages/player/player',
+      success: function (res) {
+        // console.log(res)
+        res.shareTickets // 单聊是没有的
+      }
+    }
+  }
+})
